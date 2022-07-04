4f7f4a2eec633de7b8b97247f2e76e4e42cf089a
Hervert Damian Martinez <luis2009hervert@hotmail.com>
2022-07-04 13:30:27 -0600

    Agregacion de scripts portal

---
+++ Proyecto ADII/Assets/Scripts/PlayerTeleport.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTeleport : MonoBehaviour
+{
+    public GameObject currentTeleporter;
+    // Start is called before the first frame update
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (currentTeleporter != null)
+            {
+                transform.position = currentTeleporter.GetComponent<Portal>().GetDestination().position;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Teleporter"))
+        {
+            currentTeleporter = collision.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Teleporter"))
+        {
+            if (collision.gameObject == currentTeleporter)
+            {
+                currentTeleporter = null;
+            }
+        }
+    }
+}
--- Proyecto ADII/Assets/Scripts/Portal.cs
+++ Proyecto ADII/Assets/Scripts/Portal.cs
@@ -4,18 +4,10 @@ using UnityEngine;
 
 public class Portal : MonoBehaviour
 {
-    public GameObject portal;
-    private GameObject player;
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Transform destination;
+    public Transform GetDestination()
     {
-        player = GameObject.FindWithTag("perseguido");
+        return destination;
     }
-
-    // Update is called once per frame
-    private void OnTriggerEnter2D(Collider2D collision){
-        if(collision.tag == "perseguido"){
-            player.transform.position = new Vector2(portal.transform.position.x, portal.transform.position.y);
-     }   
-    }
+    
 }
